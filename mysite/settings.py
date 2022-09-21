9a6707afbdc1a61ee12b427e93b0c86c6525842e
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-09-21 22:44:43 +0530

    ...

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'assets')
 ]
 
 MEDIA_URL = '/media/'
