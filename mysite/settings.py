a84a3672cf76fac748190c40e5335508185aa5b3
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-10-21 12:46:10 +0530

    ..

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
         'PASSWORD': '123456',
     }
 }
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
